8cfc408c3e0acffd321118a7d642cfdd25e3e30f
Lihopps <lihops.game@gmail.com>
2024-11-21 20:46:08 +0100

    Init

--- control.lua
+++ control.lua
@@ -1,9 +1,9 @@
-local handler = require("__core__.lualib.event_handler")
+-- local handler = require("__core__.lualib.event_handler")
 
-handler.add_libraries({
-  	require("__flib__.gui"),
-	require("script.main"),
-	require("script.surface"),
-	require("script.entity"),
-	require("script.teleporter_gui"),
-})
+-- handler.add_libraries({
+--   	require("__flib__.gui"),
+-- 	require("script.main"),
+-- 	require("script.surface"),
+-- 	require("script.entity"),
+-- 	require("script.teleporter_gui"),
+-- })
